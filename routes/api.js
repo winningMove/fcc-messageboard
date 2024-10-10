3c3f3c7e7ccf17c41a3fa42fc530be873ec1a6f8
winningMove <dariopapic+github@gmail.com>
2024-10-10 10:11:12 +0200

    add reply routes

--- routes/api.js
+++ routes/api.js
@@ -63,7 +63,7 @@ module.exports = function (app) {
       const { board } = req.params;
       const { thread_id } = req.query;
       try {
-        const thread = await Thread.find({ board, _id: thread_id });
+        const thread = await Thread.findOne({ board, _id: thread_id });
         thread.reported = true;
         await thread.save();
         res.send("reported");
@@ -72,5 +72,66 @@ module.exports = function (app) {
       }
     });
 
-  app.route("/api/replies/:board");
+  app
+    .route("/api/replies/:board")
+    .post(async function (req, res) {
+      const { board } = req.params;
+
+      let { text, delete_password, thread_id } = req.query;
+      delete_password = hashPassword(delete_password);
+
+      try {
+        const thread = await Thread.find({ board, _id: thread_id });
+        const reply = await Reply.create({ text, delete_password });
+        thread.bumped_on = reply.created_on;
+        thread.replies.push(reply._id);
+        await thread.save();
+        res.send("Successfully added new reply to target thread");
+      } catch (error) {
+        res.status(400).send("error");
+      }
+    })
+    .get(async function (req, res) {
+      const { board } = req.params;
+      const { thread_id } = req.query;
+      try {
+        const thread = await Thread.findOne(
+          { board, _id: thread_id },
+          "-delete_password -reported"
+        ).populate({
+          path: "replies",
+          select: "-delete_password -reported",
+        });
+        res.json(thread);
+      } catch (error) {
+        res.status(400).send("error");
+      }
+    })
+    .delete(async function (req, res) {
+      let { reply_id, delete_password } = req.query;
+      delete_password = hashPassword(delete_password);
+      try {
+        const reply = await Reply.findOne({
+          _id: reply_id,
+        });
+        if (reply.delete_password !== delete_password)
+          res.send("incorrect password");
+        reply.text = "[deleted]";
+        await reply.save();
+        res.send("success");
+      } catch (error) {
+        res.status(400).send("error");
+      }
+    })
+    .put(async function (req, res) {
+      const { reply_id } = req.query;
+      try {
+        const reply = await Reply.findOne({ _id: reply_id });
+        reply.reported = true;
+        await reply.save();
+        res.send("reported");
+      } catch (error) {
+        res.status(400).send("error");
+      }
+    });
 };
