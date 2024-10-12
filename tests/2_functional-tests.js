2a9cd4be3de0374ec07596d03044a844fffc71f4
winningMove <dariopapic+github@gmail.com>
2024-10-12 09:58:50 +0200

    cleanup

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,7 +2,6 @@ const chaiHttp = require("chai-http");
 const chai = require("chai");
 const assert = chai.assert;
 const server = require("../server");
-const crypto = require("crypto");
 const { Thread, Reply } = require("../model/ThreadModels");
 const { suiteSetup, suiteTeardown } = require("mocha");
 const { hashPassword } = require("../routes/api");
